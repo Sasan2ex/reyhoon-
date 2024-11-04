c762db30da387bc4d3273f6e3856b82cc1c1f0c3
Sasan2ex <sasan.hosseiny@yahoo.com>
2024-11-04 21:22:18 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,27 @@
-﻿
-    
-        int n = 10;
-        int m = 100 ;
+﻿int n = 10;
+int m = 1000;
 
-        for (int i = n; i < m; i++)
-        {
-            int a = 0;
-            int b = 1;
-            int c = 0;
-            bool isFibo = false;
+for (int i = n; i < m; i++)
+{
+   
+    if (IsPalindrome(i))
 
-           
-            while (c < m)
-            {
-                c = a + b;
-                if (n < c && c < m)
-                {
-                    if (i == c)
-                    {
-                        isFibo = true;
-                        break; 
-                    }
-                }
-                a = b;
-                b = c;
-            }
+    {
+        Console.WriteLine(i);
 
-            if (isFibo)
-                Console.WriteLine( i + " is a Fibonacci number");
-        }
-  
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        string str = number.ToString();
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+        return str == new string(arr);
+    }
+}
+
+
+
+        
+       
+
